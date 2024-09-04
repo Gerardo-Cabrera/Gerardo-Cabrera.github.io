7290d7902e296d764247b15c6714cce5fbaad753
Gerardo-Cabrera <gerardocabrera18@gmail.com>
2024-09-04 19:32:39 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// Crear la ruleta usando Winwheel.js
+let myWheel = new Winwheel({
+  canvasId: "ruletaCanvas",
+  numSegments: 6,
+  outerRadius: 200,
+  segments: [
+    { fillStyle: "#f1c40f", text: "Viaje" },
+    { fillStyle: "#8e44ad", text: "Televisor" },
+    { fillStyle: "#2ecc71", text: "Laptop" },
+    { fillStyle: "#e67e22", text: "Bicicleta" },
+    { fillStyle: "#e74c3c", text: "Cena para dos" },
+    { fillStyle: "#3f3ab1", text: "Vale de compras" },
+  ],
+  animation: {
+    type: "spinToStop",
+    duration: 10, // Duración del giro en segundos
+    spins: 9, // Número de vueltas completas
+    callbackFinished: "mostrarGanador()", // Llamada cuando termina el giro
+    callbackSound: "actualizarOpcion()", // Llamada en cada sonido del segmento
+    callbackAfter: "dibujarIndicador()",
+    //soundTrigger: "segment",
+  },
+});
+
+function dibujarIndicador() {
+  var ctx = myWheel.ctx;
+  ctx.strokeStyle = "navy";
+  ctx.fillStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(250, 40);
+  ctx.lineTo(230, 10);
+  ctx.lineTo(270, 10);
+  ctx.stroke();
+  ctx.fill();
+}
+
+dibujarIndicador();
+
+// Función para mostrar el ganador
+function mostrarGanador() {
+  let segment = myWheel.getIndicatedSegment();
+  document.getElementById(
+    "resultado-final"
+  ).textContent = `¡El ganador es: ${segment.text}!`;
+  document.getElementById("girar").disabled = true; // Deshabilitar botón después del giro
+  localStorage.setItem("ruletaGirada", "true"); // Guardar en localStorage
+  myWheel.stopAnimation(false);
+  myWheel.draw();
+  dibujarIndicador();
+}
+
+// Función para actualizar la opción mientras la rueda gira
+function actualizarOpcion() {
+  let segment = myWheel.getIndicatedSegment();
+  document.getElementById(
+    "opcion-actual"
+  ).textContent = `Opción actual: ${segment.text}`;
+}
+
+// Comprobar si la rueda ya fue girada
+window.onload = function () {
+  if (localStorage.getItem("ruletaGirada")) {
+    document.getElementById("girar").disabled = true;
+  }
+};
+
+// Iniciar la animación de la ruleta cuando se hace clic en el botón
+document.getElementById("girar").addEventListener("click", function () {
+  myWheel.startAnimation();
+});
+
